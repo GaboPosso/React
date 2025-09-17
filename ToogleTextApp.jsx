d88e7e0fcd7c4699f1c6e6771f2876e023331b72
Gabriel Mejía <gmejia@techpulse.com>
2025-09-17 07:59:08 -0500

    Upload ToggleTextApp UseState Hook

---
+++ ToogleTextApp.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export const ToggleApp = () => {
+    const [isVisible, setIsVisible] = useState(false);
+    return (
+    <div id="toggle-container">
+        <button id="toggle-button">Message</button>
+        <p id="message">I love freeCodeCamp!</p>
+    </div>
+    );
+}
