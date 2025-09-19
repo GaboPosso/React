3e13702b9420ffce18c71ce6d47f241cff53265e
Gabriel Mejía <gmejia@techpulse.com>
2025-09-19 02:05:18 -0500

    Upload ToogleTextApp component

--- ToogleTextApp.jsx
+++ ToogleTextApp.jsx
@@ -1,11 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export const ToggleApp = () => {
     const [isVisible, setIsVisible] = useState(false);
+
+    const handleToggleVisibility = () => {
+        setIsVisible(!isVisible);
+    }
     return (
     <div id="toggle-container">
-        <button id="toggle-button">Message</button>
-        <p id="message">I love freeCodeCamp!</p>
+        <button id="toggle-button" onClick={handleToggleVisibility}>{isVisible? "Hide" : "Show"} Message</button>
+        {isVisible && <p id="message">I love freeCodeCamp!</p>}        
     </div>
     );
 }
