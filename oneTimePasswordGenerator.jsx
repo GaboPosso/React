8b2bb80f75b744fbd4a117e80beaf42cadfdfed8
Gabriel Mejía <gmejia@techpulse.com>
2025-09-25 07:45:01 -0500

    Upload Superhero application form React exercise

--- oneTimePasswordGenerator.jsx
+++ oneTimePasswordGenerator.jsx
@@ -1,13 +1,41 @@
 const { useState, useEffect, useRef } = React;
 
 export const OTPGenerator = () => {
-    
+    const [otp, setOtp] = useState('');
+    const timerRef = useRef(null);
+    const [count, setCount] = useState(null);
+    const [ctaDisabled, setCTADisabled] = useState(false)
+
+    const generateOTP = () => {
+        if (timerRef.current) {
+            clearInterval(timerRef.current);
+        }
+        setOtp(Math.floor(100000 + Math.random() * 900000).toString());
+        setCount(5);
+
+        timerRef.current = setInterval(() => {
+            setCount(prevCount => {
+                if (prevCount <= 1) {
+                    setCTADisabled(false);
+                    clearInterval(timerRef.current);
+                    document.getElementById('otp-timer').textContent = "OTP expired. Please generate a new one.";
+                    setOtp('');
+                    return null;
+                }
+
+                document.getElementById('otp-timer').textContent = `OTP will expire in ${prevCount - 1} seconds.`;
+                return prevCount - 1;
+            })
+        }, 1000);
+        setCTADisabled(true);       
+              
+    }
     return (
         <div className="container">
             <h1 id="otp-title">OTP Generator</h1>
-            <h2 id="otp-display">Click 'Generate OTP' to get a code</h2>
+            <h2 id="otp-display">{otp ? otp : "Click 'Generate OTP' to get a code"}</h2>
             <p id="otp-timer" aria-live="assertive"></p>
-            <button id="generate-otp-button">Generate OTP</button>
+            <button id="generate-otp-button" onClick={generateOTP}>Generate OTP</button>
 
         </div>
     )
---
+++ superHeroForm.jsx
@@ -0,0 +1,43 @@
+const { useState } = React;
+
+export const SuperHeroForm = () => {
+
+    const powerSourceOptions = [
+        'Bitten by a strange creature',
+        'Radioactive exposure',
+        'Science experiment',
+        'Alien heritage',
+        'Ancient artifact discovery',
+        'Other'
+    ];
+
+    const [heroName, setHeroName] = useState("");
+    const [realName, setRealName] = useState("");
+    const [powerSource, setPowerSource] = useState("");
+    const [powers, setPowers] = useState([]);
+
+    return (
+        <div className="form-wrap">
+            <h2>Superhero Application Form</h2>
+            <p>Please complete all fields</p>
+            <form>
+                <div className="section">
+                    <label>Hero Name
+                        <input type="text" value={heroName} onChange={(e) => setHeroName(e.target.value)} />
+                    </label>
+                    <label>Real Name
+                        <input type="password" value={realName} onChange={(e) => setRealName(e.target.value)} />
+                    </label>
+                    <label className="section column">How did you get your powers?
+                        <select value={powerSource} onChange={(e) => {setPowerSource(e.target.value)}}>
+                            <option value=''>Select one</option>
+                            {powerSourceOptions.map((source) => (
+                                <option key={source} value={source}>{source}</option>
+                            ))}
+                        </select>
+                    </label>
+                </div>
+            </form>
+        </div>
+    )
+};
